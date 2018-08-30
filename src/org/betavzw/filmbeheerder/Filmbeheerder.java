207fff1ae98d71600864b013a54742c6b235a81a
Jef-DS <Jef.DeSmedt@betavzw.org>
2018-08-30 10:44:44 +0200

    export naar HTML toegevoegd

--- src/org/betavzw/filmbeheerder/Filmbeheerder.java
+++ src/org/betavzw/filmbeheerder/Filmbeheerder.java
@@ -2,12 +2,17 @@ package org.betavzw.filmbeheerder;
 
 import org.betavzw.film.BetaFilm;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Filmbeheerder {
-    private static final String[] menuItems= {"Film toevoegen", "Lijst tonen", "Film zoeken", "Stoppen"};
+    private static final String[] menuItems = {"Film toevoegen", "Lijst tonen", "Film zoeken", "Exporteer naar HTML", "Stoppen"};
     private List<BetaFilm> films;
     private Scanner scanner;
 
@@ -16,61 +21,124 @@ public class Filmbeheerder {
         this.scanner = scanner;
         toonMenu();
     }
-    private void toonMenu(){
+
+    private void toonMenu() {
         int keuze;
-        do{
-            for(int i= 0; i<menuItems.length-1;i++){
-                System.out.printf("%d. %s.%n", i+1,menuItems[i]);
+        do {
+            for (int i = 0; i < menuItems.length - 1; i++) {
+                System.out.printf("%d. %s.%n", i + 1, menuItems[i]);
             }
-            System.out.printf("%d. %s.%n", 0, menuItems[menuItems.length-1]);
+            System.out.printf("%d. %s.%n", 0, menuItems[menuItems.length - 1]);
             System.out.print("U keuze: ");
             keuze = Integer.parseInt(scanner.nextLine());
-            switch(keuze){
-                case 1: filmToevoegen();
+            switch (keuze) {
+                case 1:
+                    filmToevoegen();
                     break;
-                case 2: filmsTonen(films);
+                case 2:
+                    filmsTonen(films);
                     break;
-                case 3: filmZoeken();
+                case 3:
+                    filmZoeken();
+                    break;
+                case 4:
+                    filmsExporteren(films);
                     break;
                 case 0:
                     break;
                 default:
                     System.out.println("Ongeldige keuze");
             }
-        }while(keuze != 0);
+        } while (keuze != 0);
 
     }
-    private void filmToevoegen(){
+
+    private void filmsExporteren(List<BetaFilm> films) {
+        String htmlfile = "test.html";
+        try {
+            Path templateFile = vraagTemplateFile();
+            List<String> lijnen = Files.lines(templateFile).collect(Collectors.toList());
+            for(int i=0;i<lijnen.size();i++){
+                lijnen.set(i, lijnen.get(i).replace("%TITLE%","Filmlijst"));
+            }
+            String htmlTabel = maakhtmlTabel(films);
+            for(int i=0;i<lijnen.size();i++){
+                lijnen.set(i, lijnen.get(i).replace("%BODY%",htmlTabel));
+            }
+            try(PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(htmlfile)))){
+                for (String lijn: lijnen){
+                    writer.println(lijn);
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private String maakhtmlTabel(List<BetaFilm> films) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<table>");
+        sb.append("<tr><th>Titel</th><th>Regisseur</th><th>Jaartal</th></tr>");
+        for(BetaFilm film: films){
+            sb.append("<tr>");
+            sb.append("<td>"+film.getTitel()+"</td>");
+            sb.append("<td>"+film.getRegisseur()+"</td>");
+            sb.append("<td>"+film.getJaartal()+"</td>");
+            sb.append("</tr>");
+        }
+        sb.append("</table>");
+        return sb.toString();
+    }
+
+    private Path vraagTemplateFile() throws IOException {
+        Path currentDirectory = Paths.get("").toAbsolutePath();
+        List<Path> bestanden = Files.walk(currentDirectory, 1)
+                .filter(Files::isRegularFile)
+                .filter(path -> path.toString().endsWith(".thtml"))
+                .collect(Collectors.toList());
+        if (bestanden.size() == 1) {
+            return bestanden.get(0);
+        }
+        for (Path p : bestanden) {
+            System.out.println(p);
+        }
+        return null;
+    }
+
+    private void filmToevoegen() {
         System.out.print("Geef titel: ");
         String titel = scanner.nextLine();
         System.out.print("Geef regisseur: ");
         String regisseur = scanner.nextLine();
         System.out.print("Geef jaartal: ");
         int jaartal = Integer.parseInt(scanner.nextLine());
-        BetaFilm film = new BetaFilm(films.size(),titel, regisseur,jaartal);
+        BetaFilm film = new BetaFilm(films.size(), titel, regisseur, jaartal);
         films.add(film);
     }
-    private void filmsTonen(List<BetaFilm> films){
-        System.out.printf("%3s %25s %20s%10s%n","id", "titel", "regisseur", "jaartal");
-        for(BetaFilm film: films){
-            System.out.printf("%3d %25s %20s%10d%n", film.getId(), fittoLength(film.getTitel(), 25), fittoLength(film.getRegisseur(),20), film.getJaartal());
+
+    private void filmsTonen(List<BetaFilm> films) {
+        System.out.printf("%3s %25s %20s%10s%n", "id", "titel", "regisseur", "jaartal");
+        for (BetaFilm film : films) {
+            System.out.printf("%3d %25s %20s%10d%n", film.getId(), fittoLength(film.getTitel(), 25), fittoLength(film.getRegisseur(), 20), film.getJaartal());
         }
         System.out.print("Druk <RETURN> om verder te gaan.");
         scanner.nextLine();
     }
-    private String fittoLength(String tekst, int maxlen){
-        if (tekst.length()> maxlen){
-            return tekst.substring(0, maxlen-4) + "... ";
-        }else{
+
+    private String fittoLength(String tekst, int maxlen) {
+        if (tekst.length() > maxlen) {
+            return tekst.substring(0, maxlen - 4) + "... ";
+        } else {
             return tekst;
         }
     }
-    private void filmZoeken(){
+
+    private void filmZoeken() {
         System.out.print("Geef een deel van de titel die je wil zoeken: ");
         String zoektekst = scanner.nextLine();
         List<BetaFilm> gevonden = new ArrayList<>();
-        for(BetaFilm film: films){
-            if (film.getTitel().contains(zoektekst)){
+        for (BetaFilm film : films) {
+            if (film.getTitel().contains(zoektekst)) {
                 gevonden.add(film);
             }
         }
